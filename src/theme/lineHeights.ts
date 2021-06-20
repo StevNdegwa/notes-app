f2f8733c9c5f557a3291e64ad4f491434634d11e
Stephen N <sndegwa.n@outlook.com>
2021-06-20 21:11:30 +0300

    update theme

--- src/theme/lineHeights.ts
+++ src/theme/lineHeights.ts
@@ -8,14 +8,14 @@ export const lineHeights = {
     base: 1.5,
     tall: 1.625,
     taller: "2",
-    "3": space["3"],
-    "4": space["4"],
-    "5": space["5"],
-    "6": space["6"],
-    "7": space["7"],
-    "8": space["8"],
-    "9": space["9"],
-    "10": space["10"],
+    3: space["3"],
+    4: space["4"],
+    5: space["5"],
+    6: space["6"],
+    7: space["7"],
+    8: space["8"],
+    9: space["9"],
+    10: space["10"],
 }
 
 export type LineHeights = typeof lineHeights;
