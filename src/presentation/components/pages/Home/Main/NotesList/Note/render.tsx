491b22e3a887f254e47eeee2a29b5f47c0bc2ad0
Stephen N <sndegwa.n@outlook.com>
2021-11-09 19:49:42 +0300

    fix: parse html content

--- src/presentation/components/pages/Home/Main/NotesList/Note/render.tsx
+++ src/presentation/components/pages/Home/Main/NotesList/Note/render.tsx
@@ -1,27 +1,35 @@
 import { OutputBlockData } from "@editorjs/editorjs";
-import { NotesHeading, NotesOrderedList, NotesParagraph, NotesUnorderedList } from "./styles";
+import parse from "html-react-parser";
+import {
+  NotesHeading,
+  NotesOrderedList,
+  NotesParagraph,
+  NotesUnorderedList,
+} from "./styles";
 
 export default function render(block: OutputBlockData, index: number) {
   switch (block.type) {
     case "header":
-      return <NotesHeading key={index}>{block.data.text}</NotesHeading>;
+      return <NotesHeading key={index}>{parse(block.data.text)}</NotesHeading>;
     case "paragraph":
-      return <NotesParagraph key={index}>{block.data.text}</NotesParagraph>;
+      return (
+        <NotesParagraph key={index}>{parse(block.data.text)}</NotesParagraph>
+      );
     case "list":
       return block.data.style === "ordered" ? (
         <NotesOrderedList key={index}>
           {block.data.items.map((item: string, index: number) => (
-            <li key={index}>{item}</li>
+            <li key={index}>{parse(item)}</li>
           ))}
         </NotesOrderedList>
       ) : (
         <NotesUnorderedList key={index}>
           {block.data.items.map((item: string, index: number) => (
-            <li key={index}>{item}</li>
+            <li key={index}>{parse(item)}</li>
           ))}
         </NotesUnorderedList>
       );
     default:
-      return <div key={index}>{block.data.text}</div>;
+      return <div key={index}>{parse(block.data.text)}</div>;
   }
 }
